9466f1deb6ce0a19bdac5924b18de5f1582a13be
xeronimus <xeronimus@gmail.com>
2014-09-30 09:13:04 +0200

    added eslint to build. fixed eslint warnings

--- app/app.js
+++ app/app.js
@@ -1,56 +1,58 @@
-(function(){
+(function () {
   'use strict';
 
-  angular.module("JSEnterpriseWorkshopApp", ['ui.router'])
+  angular.module('JSEnterpriseWorkshopApp', ['ui.router'])
 
-  .config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider
+    .config(function ($stateProvider, $urlRouterProvider) {
+      $stateProvider
         .state('wizard', {
-            url: '/wizard',
-            templateUrl: 'templates/wizard.html',
-            controller: 'WizardCtrl as ctrl'
+          abstract: true,
+          url: '/wizard',
+          templateUrl: 'templates/wizard.html',
+          controller: 'WizardCtrl as ctrl'
         })
         .state('wizard.product', {
-            url: '/product',
-            templateUrl: 'templates/wizard-product.html'
+          url: '/product',
+          templateUrl: 'templates/wizard-product.html'
         })
         .state('wizard.date', {
-            url: '/date',
-            templateUrl: 'templates/wizard-date.html'
+          url: '/date',
+          templateUrl: 'templates/wizard-date.html'
         })
         .state('wizard.contactdetails', {
-            url: '/contactdetails',
-            templateUrl: 'templates/wizard-contactdetails.html'
+          url: '/contactdetails',
+          templateUrl: 'templates/wizard-contactdetails.html'
         })
         .state('wizard.confirmation', {
-            url: '/confirmation',
-            templateUrl: 'templates/wizard-confirmation.html'
+          url: '/confirmation',
+          templateUrl: 'templates/wizard-confirmation.html'
         });
 
-    $urlRouterProvider.otherwise('/wizard/product');
-  })
+      $urlRouterProvider.otherwise('/wizard/product');
+    })
 
-  .controller("WizardCtrl", function(appointmentService){
-    var vm = this;
-    vm.appointment = {};
-    vm.products = appointmentService.getProducts();
-    vm.getProposalForDate = appointmentService.getProposalForDate;
-  })
+    .controller('WizardCtrl', function (appointmentService) {
+      var vm = this;
+      vm.appointment = {};
+      vm.products = appointmentService.getProducts();
+      vm.getProposalForDate = appointmentService.getProposalForDate;
+    })
 
-  .factory('appointmentService', function() {
-    function getProducts() {
-      return [
-        {"id":"herren", "label":"Schnitt Herren (45.00 CHF)"},
-        {"id":"damen",  "label":"Schnitt Damen (60.00 CHF)"}];
-    }
+    .factory('appointmentService', function () {
+      function getProducts() {
+        return [
+          {'id': 'herren', 'label': 'Schnitt Herren (45.00 CHF)'},
+          {'id': 'damen', 'label': 'Schnitt Damen (60.00 CHF)'}
+        ];
+      }
 
-    function getProposalForDate(selectedDate) {
-      return ["15.15", "16.00", "16.15", "17.45"];
-    }
+      function getProposalForDate() {
+        return ['15.15', '16.00', '16.15', '17.45'];
+      }
 
-    return {
-      getProducts: getProducts,
-      getProposalForDate: getProposalForDate
-    };
-  });
+      return {
+        getProducts: getProducts,
+        getProposalForDate: getProposalForDate
+      };
+    });
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,39 @@
-var gulp      = require('gulp');
+var gulp = require('gulp');
+var eslint = require('gulp-eslint');
 var webserver = require('gulp-webserver');
 
 var server = {
   host: 'localhost',
   port: '8001',
   root: './app'
-}
+};
 
-gulp.task('webserver', function() {
-  gulp.src( server.root )
+
+var path = {
+  ourJsFiles: ['app/**/*.js', 'test/**/*.js', '!app/lib/**/*']
+
+};
+
+gulp.task('webserver', ['lint'], function () {
+  gulp.src(server.root)
     .pipe(webserver({
-      host:             server.host,
-      port:             server.port,
-      open:             true,
-      livereload:       true,
+      host: server.host,
+      port: server.port,
+      open: true,
+      livereload: true,
       directoryListing: false
     }));
+
+  gulp.watch(path.ourJsFiles, ['lint']);
 });
+
+
+gulp.task('lint', function () {
+  gulp.src(path.ourJsFiles)
+    .pipe(eslint())
+    .pipe(eslint.format());
+
+});
+
 
 gulp.task('default', ['webserver']);
